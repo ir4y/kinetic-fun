c5a963b9e5fa808e533fedc81d1d947cb9aa42bb
Ilya Beda <ir4y.ix@gmail.com>
2013-06-06 13:47:30 +0800

    Some fixtures

--- src/clj/kinetic_fun/core.clj
+++ src/clj/kinetic_fun/core.clj
@@ -13,12 +13,13 @@
 (defn setup-new-coords [coords]
   (let [data (json/parse-string coords)]
     (model/publish-circle coords)
-    (session/put! "circle-x" (data "x"))
-    (session/put! "circle-y" (data "y"))))
+    (session/put! :circle-x (data "x"))
+    (session/put! :circle-y (data "y"))))
 
 
 (defn ws_handler [request]
-  (session/put! 1 "a")
+  (session/put! :value "ws")
+  (println (session/get! :value))
   (kit/with-channel request channel
     (kit/on-close channel (fn [status]                            
                             (println "channel closed: " status)))
@@ -27,8 +28,8 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/push" [value] (println (cookies/get :ring-session)) (session/put! 1 value) "pushed")
-  (GET "/pop" [] (str "pop:" (session/get 1)))
+  (GET "/push" [value] (println (cookies/get :ring-session)) (session/put! :value value) "pushed")
+  (GET "/pop" [] (str "pop:" (session/get :value)))
   (GET "/ws" [] ws_handler)
   (route/resources "/")
   (route/not-found "Not Found"))
@@ -36,4 +37,4 @@
 (defn -main [& args]
   (kit/run-server  (session/wrap-noir-session
                      (cookies/wrap-noir-cookies
-                       (handler/site app-routes)) {:session-store model/redis-session-store}) {:port 3000}))
+                       (handler/site app-routes)) {:store model/redis-session-store}) {:port 3000}))
